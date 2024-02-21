6ff24a956435c5ea9d9f365209b85a5c0b7d079f
Forever Inc dev <redodevs@gmail.com>
2024-02-21 17:55:39 +0300

    Update LoginPage.tsx

--- src/pages/Accounts/LoginPage.tsx
+++ src/pages/Accounts/LoginPage.tsx
@@ -24,7 +24,7 @@ const LoginPage = () => {
         e.preventDefault();
         const username = e.currentTarget.username.value;
         const password = e.currentTarget.password.value;
-        await LoginUser(username, password);
+        await LoginUser(username, password)
     };
 
     return (
