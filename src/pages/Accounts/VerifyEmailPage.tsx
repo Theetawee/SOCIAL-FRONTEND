116dd409ef26c6ea089743dcfe6e24a4922dd880
Forever Inc dev <redodevs@gmail.com>
2024-02-22 09:24:10 +0300

    Update VerifyEmailPage.tsx

--- src/pages/Accounts/VerifyEmailPage.tsx
+++ src/pages/Accounts/VerifyEmailPage.tsx
@@ -38,7 +38,7 @@ const VerifyEmailPage = () => {
             description="Activate your account to access exclusive features, connect with friends, and explore a world of possibilities. Simply verify your email address to begin your journey. Let's get started!"
         >
             <section className="h-screen px-4 flex items-center justify-center">
-                <div className="max-w-md w-full min-h-52 mx-auto dark:bg-gray-800 py-10 bg-white border-gray-100 shadow px-4 sm:px-6 rounded">
+                <div className="max-w-lg w-full min-h-52 mx-auto py-10  border-gray-100 shadow px-4 sm:px-6 rounded">
                     <div className="w-full">{content}</div>
                 </div>
             </section>
