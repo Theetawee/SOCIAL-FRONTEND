1f18877ee065c786f47d44bd5ca2727aa96d9fe4
Forever Inc dev <redodevs@gmail.com>
2024-02-23 22:15:26 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,20 @@ const LoginPage = lazy(() => import("./pages/Accounts/LoginPage"));
 const LogoutPage = lazy(() => import("./pages/Accounts/LogoutPage"));
 const SignUpPage = lazy(() => import("./pages/Accounts/SignUpPage"));
 const VerifyEmailPage = lazy(() => import("./pages/Accounts/VerifyEmailPage"));
-const GoogleLoginPage = lazy(() => import('./pages/Accounts/GoogleLoginPage'));
+const GoogleLoginPage = lazy(() => import("./pages/Accounts/GoogleLoginPage"));
 const ProfilePage = lazy(() => import("./pages/Accounts/ProfilePage"));
-const PostDetailPage = lazy(() => import('./pages/Posts/PostDetailPage'));
+const PostDetailPage = lazy(() => import("./pages/Posts/PostDetailPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
-const ResetPasswordPage= lazy(() => import('./pages/Accounts/ResetPasswordPage'));
-const PasswordResetConfirmPage= lazy(() => import('./pages/Accounts/PasswordResetConfirmPage'));
-const AccountActivationPage= lazy(() => import('./pages/Accounts/AccountActivationPage'));
+const ResetPasswordPage = lazy(
+    () => import("./pages/Accounts/ResetPasswordPage")
+);
+const PasswordResetConfirmPage = lazy(
+    () => import("./pages/Accounts/PasswordResetConfirmPage")
+);
+const AccountActivationPage = lazy(
+    () => import("./pages/Accounts/AccountActivationPage")
+);
 const ComposePage = lazy(() => import("./pages/Posts/ComposePage"));
-
-
-
 
 const router = createBrowserRouter(
     createRoutesFromElements([
@@ -37,21 +40,38 @@ const router = createBrowserRouter(
                 <Route path="/compose" element={<ComposePage />} />
                 <Route path="/:username" element={<ProfilePage />} />
                 <Route path="/posts/:id" element={<PostDetailPage />} />
-          <Route path="/logout" element={<LogoutPage />} />
+                <Route path="/logout" element={<LogoutPage />} />
             </Route>
         </Route>,
         <Route path="/accounts" element={<MainLayout />}>
             <Route element={<ProtectedRoute />}>
-            <Route path="/accounts/login" element={<LoginPage />} />
-            <Route path="/accounts/signup" element={<SignUpPage />} />
-            <Route path="/accounts/verify-email" element={<VerifyEmailPage />} />
-                <Route path="/accounts/oauth2/google" element={<GoogleLoginPage />} />
-                <Route path="/accounts/reset-password" element={<ResetPasswordPage />} />
-                <Route path="/accounts/password/reset/confirm/:uid/:token" element={<PasswordResetConfirmPage />} />
-                <Route path="/accounts/activate/:token" element={<AccountActivationPage />} />
+                <Route path="/accounts/login" element={<LoginPage />} />
+                <Route path="/accounts/signup" element={<SignUpPage />} />
+                <Route
+                    path="/accounts/verify-email"
+                    element={<VerifyEmailPage />}
+                />
+                <Route
+                    path="/accounts/oauth2/google"
+                    element={<GoogleLoginPage />}
+                />
+                <Route
+                    path="/accounts/reset-password"
+                    element={<ResetPasswordPage />}
+                />
+                <Route
+                    path="/accounts/password/reset/confirm/:uid/:token"
+                    element={<PasswordResetConfirmPage />}
+                />
+                <Route
+                    path="/accounts/activate/:token"
+                    element={<AccountActivationPage />}
+                />
             </Route>
         </Route>,
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<MainLayout />}>
+            <Route path="*" element={<NotFoundPage />} />
+        </Route>,
     ])
 );
 
