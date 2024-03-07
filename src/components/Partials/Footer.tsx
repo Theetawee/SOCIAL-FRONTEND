41f9d4f127f60d787851901e5d7f9e9fd9b0aefa
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-07 17:36:10 +0300

    Update Footer.tsx

--- src/components/Partials/Footer.tsx
+++ src/components/Partials/Footer.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
-      <div>
-        <footer className="flex bg-gray-50 text-sm border border-gray-300 dark:border-gray-800 rounded-full px-6 italic my-4 dark:bg-gray-950 w-full  items-center gap-8 flex-wrap justify-between max-w-screen-lg   mx-auto p-4">
+      <div className="px-3">
+        <footer className="flex bg-gray-50 justify-center text-sm border border-gray-300 dark:border-gray-800 rounded-full px-6 italic my-4 dark:bg-gray-950 w-full  items-center gap-x-8 flex-wrap sm:justify-between max-w-screen-lg  gap-y-2 mx-auto p-4">
           <span>
             <Link to={"/"} className="hover:underline hover:text-primary-600">
               Home
