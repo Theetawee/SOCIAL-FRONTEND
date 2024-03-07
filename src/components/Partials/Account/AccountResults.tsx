cb7798147207bea1adc10e4efd7a3196922c5b5a
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-07 11:03:17 +0300

    save

--- src/components/Partials/Account/AccountResults.tsx
+++ src/components/Partials/Account/AccountResults.tsx
@@ -4,11 +4,16 @@ const AccountResults = ({ data }: { data: SuggestedAccount[] }) => {
     console.log(data)
   return (
       <div className="grid grid-cols-1 max-w-2xl mx-auto gap-3">
-          {data.map((account) => (
+          {data.length>0 ?(<>{data.map((account) => (
               <div key={account.id} className="dark:bg-gray-900 bg-gray-50 p-4 rounded-xl">
                 <AccountCard account={account}/>  
               </div>
-          ))}
+          ))}</>) : (<>
+          
+          <p className="text-center text-lg">No Accounts found.</p>
+          
+          </>)}
+          
     </div>
   )
 }
--- src/components/Partials/Post/PostResults.tsx
+++ src/components/Partials/Post/PostResults.tsx
@@ -4,14 +4,20 @@ import Post from "./Post";
 const PostResults = ({ data }: { data: PostType[] }) => {
     console.log(data);
   return (
-      <div className="grid max-w-2xl mx-auto grid-cols-1 gap-3">
+    <div className="grid max-w-2xl mx-auto grid-cols-1 gap-3">
+      {data.length > 0 ? (
+        <>
           {data.map((post) => (
-              <div key={post.id}>
-                  <Post post={post}/>
-              </div>
+            <div key={post.id}>
+              <Post post={post} />
+            </div>
           ))}
+        </>
+      ) : (
+        <><p className="text-center text-lg">No Posts found.</p></>
+      )}
     </div>
-  )
+  );
 }
 
 export default PostResults
