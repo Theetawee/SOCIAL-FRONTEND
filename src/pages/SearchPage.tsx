1efcc102655728d3f3e7338947acea5e20c7bd47
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-07 18:21:23 +0300

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -9,22 +9,17 @@ import AccountResults from "../components/Partials/Account/AccountResults";
 import PostResults from "../components/Partials/Post/PostResults";
 
 const SearchPage = () => {
-
-  const { searchWaanverse} = Endpoints();
+  const { searchWaanverse } = Endpoints();
 
   const [searchParams] = useSearchParams();
 
-  const query = searchParams.get('q')
-  
-
+  const query = searchParams.get("q");
 
   const [display, setDisplay] = useState("posts");
-  
-  
 
-  const { isLoading,data,isError} = useQuery({
-    queryKey: ['search', query],
-    queryFn:()=>searchWaanverse(query!),
+  const { isLoading, data, isError } = useQuery({
+    queryKey: ["search", query],
+    queryFn: () => searchWaanverse(query!),
   });
 
   let content;
@@ -33,51 +28,92 @@ const SearchPage = () => {
       <section className="py-32 px-4">
         <div>
           <Loader />
-          <p className="text-center text-lg mt-3">Searching Waanverse for "{query}"</p>
+          <p className="text-center text-lg mt-3">
+            Searching Waanverse for "{query}"
+          </p>
         </div>
       </section>
-    )
+    );
   } else if (isError) {
     content = (
       <section className="py-32 px-4">
         <div>
-          <p className="text-center text-xl text-red-400 mb-4">Something went wrong. Please try again!</p>
+          <p className="text-center text-xl text-red-400 mb-4">
+            Something went wrong. Please try again!
+          </p>
         </div>
-        <IntroSearch/>
-      </section>
-    )
-  } else { 
-    content = (<>
-      <div className="py-8">
         <IntroSearch />
-      </div>
-      <div className="max-w-xl border-x border-gray-200 dark:border-gray-700 rounded shadow mx-auto">
-      <div className="flex text-lg items-center py-2 justify-between max-w-lg mx-auto gap-10">
-
-        <div>
-            <button className={`text-center ${display === "posts" ? "text-primary-500 font-medium" : ""}`} onClick={() => setDisplay("posts")}>Posts <span className={ `ml-2 ${display==='posts'?"text-gray-600 dark:text-white":"text-gray-500"}`}>{data?.posts.length }</span></button>
-        </div>
-        <div>
-            <button className={`text-center ${display === "accounts" ? "text-primary-500 font-medium" : ""}`} onClick={() => setDisplay("accounts")}>Accounts <span className={ `ml-2 ${display==='accounts'?"text-gray-600 dark:text-white":"text-gray-500"}`}> { data?.accounts.length} </span></button>
-        </div>
-
-      </div><hr className="h-px border-t border-gray-200 dark:border-gray-800"/>
-      <div>
-          {display==="accounts"?(<AccountResults data={data!.accounts}/>):(<PostResults data={data!.posts}/>)}
-      </div></div></>
-    )
-  }
-    
-  
-
-
-    return (
-      <Seo title="Search - Waanverse" description="Search anything within Waanverse without any borders."><section className="p-4 min-h-screen">
-        {content}
-        </section>
-      </Seo>
+      </section>
     );
-  
+  } else {
+    content = (
+      <>
+        <div className="py-8">
+          <IntroSearch />
+        </div>
+        <div className="max-w-xl border-x border-gray-200 dark:border-gray-700 rounded shadow mx-auto">
+          <div className="flex text-lg items-center py-2 justify-between max-w-lg mx-auto gap-10">
+            <div>
+              <button
+                className={`text-center ${
+                  display === "posts" ? "text-primary-500 font-medium" : ""
+                }`}
+                onClick={() => setDisplay("posts")}
+              >
+                Posts{" "}
+                <span
+                  className={`ml-2 ${
+                    display === "posts"
+                      ? "text-gray-600 dark:text-white"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {data?.posts.length}
+                </span>
+              </button>
+            </div>
+            <div>
+              <button
+                className={`text-center ${
+                  display === "accounts" ? "text-primary-500 font-medium" : ""
+                }`}
+                onClick={() => setDisplay("accounts")}
+              >
+                Accounts{" "}
+                <span
+                  className={`ml-2 ${
+                    display === "accounts"
+                      ? "text-gray-600 dark:text-white"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {" "}
+                  {data?.accounts.length}{" "}
+                </span>
+              </button>
+            </div>
+          </div>
+          <hr className="h-px border-t border-gray-200 dark:border-gray-800" />
+          <div>
+            {display === "accounts" ? (
+              <AccountResults data={data!.accounts} />
+            ) : (
+              <PostResults data={data!.posts} />
+            )}
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <Seo
+      title="Search - Waanverse"
+      description="Search anything within Waanverse without any borders."
+    >
+      <section className="p-4 min-h-screen">{content}</section>
+    </Seo>
+  );
 };
 
 export default SearchPage;
