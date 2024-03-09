9e4dfb8f935898187fec39f09da16ebfe1483c4d
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-09 19:08:46 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import useSidebar from "../hooks/useSidebar";
 import FriendRequest from "../components/Partials/Sidebars/FriendRequest";
 import PopularHashtags from "../components/Partials/Sidebars/PopularHashtags";
 import Posts from "../components/Partials/Home/Posts";
 
 const HomePage = () => {
-    const [all, setAll] = useState(true);
     const { setComponent } = useSidebar();
 
     useEffect(() => {
@@ -22,21 +21,6 @@ const HomePage = () => {
 
     return (
         <section className="relative">
-            <div className="grid  px-4 py-3  text-center grid-cols-2">
-                <button
-                    className={`${all ? "text-primary-500 font-medium" : ""}`}
-                    onClick={() => setAll(true)}
-                >
-                    All Posts
-                </button>
-                <button
-                    className={`${!all ? "text-primary-500 font-medium" : ""}`}
-                    onClick={() => setAll(false)}
-                >
-                    My Friends
-                </button>
-            </div>
-            <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
             <div>
                 <Posts />
             </div>
