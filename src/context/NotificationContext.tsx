d5238650a7b61880bf5d032009cf83cd5e9dfdef
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-03-18 22:57:37 +0300

    Update NotificationContext.tsx

--- src/context/NotificationContext.tsx
+++ src/context/NotificationContext.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, createContext, useState } from "react";
-
+import { ReactNode, createContext, useEffect, useState } from "react";
+import useAuth from "../hooks/Auth/useAuth";
 
 interface NotificationContextType {
     notifications: number
@@ -12,7 +12,43 @@ export const NotificationContext = createContext<NotificationContextType>({
 
 const NotificationProvider = ({ children }: { children: ReactNode }) => {
 
-    const [notifications] = useState<number>(1);
+    const { isAuthenticated} = useAuth();
+
+    
+    const [notifications,setNotifications] = useState<number>(1);
+
+    useEffect(() => {   
+        if (isAuthenticated) {
+            const NotificationUrl =
+                process.env.NODE_ENV === "production"
+                    ? "wss://abs.waanverse.com/ws/notifications/"
+                    : "ws://localhost:8001/ws/notifications/";
+
+            const socket = new WebSocket(NotificationUrl);
+        
+
+            socket.onopen = () => {
+            };
+
+            socket.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+
+                setNotifications(data.count)
+            };
+
+            socket.onclose = () => {
+                console.log("WebSocket connection closed.");
+            };
+
+            // Cleanup function to close WebSocket connection when component unmounts
+            return () => {
+                if (socket && socket.readyState === WebSocket.OPEN) {
+                    socket.close();
+                }
+            };
+        }
+
+    }, [isAuthenticated]);
 
     const context_data = {
         notifications,
