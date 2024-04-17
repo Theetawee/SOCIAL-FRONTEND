9471f77678d6776e3b4bc51fd2c3cd103d30ba7c
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-04-17 18:08:10 +0300

    save

--- src/components/Partials/Account/LoginDrawer.tsx
+++ src/components/Partials/Account/LoginDrawer.tsx
@@ -1,81 +1,98 @@
+import { Link } from 'react-router-dom';
+import Button from '../../common/Button';
+import Input from '../../common/Input';
+import useLogin from '../../../hooks/Auth/useLogin';
+import { FormEvent } from 'react';
 const LoginDrawer = () => {
+
+
+    const { loging: isLoading, LoginUser } = useLogin();
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const username = e.currentTarget.username.value;
+      const password = e.currentTarget.password.value;
+      await LoginUser(username, password);
+    };
+  
+
+
+
   return (
-    <div>
+    <div onClick={(e) => e.stopPropagation()}>
       {/* drawer component */}
       <div
         id="drawer-bottom-example"
-        className="fixed bottom-0 left-0 right-0 z-40 w-full p-4 overflow-y-auto transition-transform bg-white dark:bg-gray-800 transform-none"
+        className="fixed bottom-0 text-gray-800 left-0 right-0 z-40 w-full p-4 overflow-y-auto transition-transform bg-gray-100 transform-none"
         tabIndex={-1}
         aria-labelledby="drawer-bottom-label">
-        <h5
-          id="drawer-bottom-label"
-          className="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">
-          <svg
-            className="w-4 h-4 me-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-          </svg>
-          Bottom drawer
-        </h5>
-        <button
-          type="button"
-          data-drawer-hide="drawer-bottom-example"
-          aria-controls="drawer-bottom-example"
-          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white">
-          <svg
-            className="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14">
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            <form onSubmit={handleSubmit} autoComplete="off" method="post">
+            <input
+              autoComplete="false"
+              name="hidden"
+              type="text"
+              style={{ display: "none" }}
             />
-          </svg>
-          <span className="sr-only">Close menu</span>
-        </button>
-        <p className="max-w-lg mb-6 text-sm text-gray-500 dark:text-gray-400">
-          Supercharge your hiring by taking advantage of our{" "}
-          <a
-            href="#"
-            className="text-blue-600 underline font-medium dark:text-blue-500 hover:no-underline">
-            limited-time sale
-          </a>{" "}
-          for Flowbite Docs + Job Board. Unlimited access to over 190K
-          top-ranked candidates and the #1 design job board.
-        </p>
-        <a
-          href="#"
-          className="px-4 py-2 me-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-          Learn more
-        </a>
-        <a
-          href="#"
-          className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-          Get access{" "}
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10">
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
-      </div>
+            <div className="grid grid-cols-1 max-w-md bg-white py-4 px-8 rounded-md shadow border border-gray-100 mx-auto">
+              <div className="py-4">
+                <div className='text-gray-700'>
+                  <p className="text-3xl mb-2">
+                    Sign in
+                  </p>
+                  <p>Stay updated on your social life</p>
+                </div>
+
+                
+              </div>
+              <div>
+                <div>
+                  <div className="grid grid-cols-1 gap-6 py-4">
+                      <Input
+                        type="text"
+                        name="username"
+                        label="Username or Email"
+                        disabled={isLoading}
+                        auto_on={true}
+                        className="mx-2 rounded-full bg-white"
+                      />
+                      <Input
+                        type="password"
+                        name="password"
+                        label="Password"
+                        disabled={isLoading}
+                        auto_on={false}
+                        className="bg-white mx-2 rounded"
+                      />
+                      <div>
+                      <Link
+                          to={"/accounts/reset-password"}
+                          className=" text-primary-500 hover:underline">
+                          Forgot Password?
+                        </Link>
+                      </div>
+                    <div>
+                      <Button
+                        type="submit"
+                        disabled={isLoading}
+                        className="w-full text-white bg-primary-600 hover:bg-primary-600/90 dark:bg-primary-700 dark:hover:bg-primary-700/90 font-medium rounded  px-5 py-2.5 text-center"
+                        label="Log in"
+                      />
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-center my-4 gap-x-2">
+              <p>New to Waanverse?</p>
+              <Link
+                            to={"/accounts/signup"}
+                            className="text-primary-500 hover:underline">
+                            Join now
+                          </Link>
+            </div>
+          </form>
+
+    </div>
     </div>
   );
 };
--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -5,45 +5,53 @@ import { useTranslation } from "react-i18next";
 import Waan from "../assets/waan.webp";
 import GoogleBtn from "../components/Partials/Account/GoogleBtn";
 import LoginDrawer from "../components/Partials/Account/LoginDrawer";
-
+import { useState } from "react";
 
 const Intro = () => {
   const { t } = useTranslation();
+
+  const [loginOpen, setLoginOpen] = useState(false);
 
   return (
     <Seo
       title="Belonging without borders - Waanverse"
       description="Discover a world of endless possibilities as you navigate through diverse communities, vibrant discussions, and captivating content. From art and culture to technology and lifestyle, there's something for everyone on Waanverse.">
-      <section >
+      <section>
         <div className="px-6 h-full  w-full">
-        <section  className="flex items-center max-w-screen-lg py-20 mx-auto justify-between">
-          <div className="flex flex-wrap sm:flex-nowrap gap-y-10 items-center h-full justify-center gap-x-10">
-            <img src={Waan} alt="Waanverse Post" className="w-80 mx-auto h-full rounded-xl shadow" />
-            <div className="mt-4 flex items-center justify-center flex-col gap-y-3 md:mt-0">
-              <h1 className="mb-10 md:text-4xl text-center text-3xl text-gray-800 dark:text-white">
-                Connecting people through meaningful connections
-              </h1>
-              <div className="grid grid-cols-3 gap-5">
-                <Link
-                  to="/accounts/signup"
-                  className="justify-center text-gray-800 flex max-w-sm w-full items-center dark:text-white border border-gray-500 dark:border-gray-600 font-medium rounded text-lg px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                  {t("Join")}
-                </Link>
-                <Link
-                  to="/accounts/login"
-                  className="justify-center flex max-w-sm w-full  items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded text-lg px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                  {t("Login")}
-                </Link>
-                <div className="max-w-sm mx-auto w-full">
+          <section className="flex items-center max-w-screen-lg py-20 mx-auto justify-between">
+            <div className="flex flex-wrap sm:flex-nowrap gap-y-10 items-center h-full justify-center gap-x-10">
+              <img
+                src={Waan}
+                alt="Waanverse Post"
+                className="w-80 mx-auto h-full rounded-xl shadow"
+              />
+              <div className="mt-4 flex items-center justify-center flex-col gap-y-3 md:mt-0">
+                <h1 className="mb-10 md:text-4xl text-center text-3xl text-gray-800 dark:text-white">
+                  Connecting people through meaningful connections
+                </h1>
+                <div className="grid grid-cols-3 gap-5">
+                  <Link
+                    to="/accounts/signup"
+                    className="justify-center text-gray-800 flex max-w-sm w-full items-center dark:text-white border border-gray-500 dark:border-gray-600 font-medium rounded text-lg px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                    {t("Join")}
+                  </Link>
+                  <button
+                    onClick={()=>{setLoginOpen((prev) => !prev)}}
+                    className="justify-center flex max-w-sm w-full  items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded text-lg px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                    Login
+                  </button>
+                  <div className="max-w-sm mx-auto w-full">
                     <GoogleBtn />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
         </div>
       </section>
-      <LoginDrawer/>
+      <div onClick={()=>{setLoginOpen((prev) => !prev)}} className={`${loginOpen ? "block" : "hidden"} fixed top-0 left-0 z-10 w-full h-screen bg-black/50 ` }>
+        <LoginDrawer />
+      </div>
     </Seo>
   );
 };
