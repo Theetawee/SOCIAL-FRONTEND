42f4f376e3f9ddfe663b1026e3fad0f90c0a1879
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-05-01 13:38:21 +0300

    save

--- src/layouts/Frame.tsx
+++ src/layouts/Frame.tsx
@@ -20,7 +20,7 @@ const Frame = () => {
                 aria-label="Sidebar"
             >
                 {/* Sidebar Content */}
-                <div className="h-full px-3 relative sm:pt-20 pt-6   bg-gray-100 dark:bg-gray-950">
+                <div className="h-full px-3 relative pt-6   bg-gray-100 dark:bg-gray-950">
                     <Aside/>
                 </div>
             </aside>
--- src/layouts/FrameLayout.tsx
+++ src/layouts/FrameLayout.tsx
@@ -1,49 +1,24 @@
 import { Outlet } from "react-router-dom";
-import { lazy } from "react";
-import ContextProvider from "../context/ContextProvider";
-import { ErrorBoundary } from "react-error-boundary";
-const CommonError = lazy(() => import("../components/common/CommonError"));
-const AppBar = lazy(() => import("./AppBar"));
-const Frame = lazy(() => import("./Frame"));
-const SideBar = lazy(() => import("./SideBar"));
-import SuspenseLoader from "../components/utils/SuspenseLoader";
 import { ThemeProvider } from "../context/ThemeContext";
+import ContextProvider from "../context/ContextProvider";
+import AppBar from "./AppBar";
+import Frame from "./Frame";
+import SideBar from "./SideBar";
 const FrameLayout = () => {
-
-    
-
-    return (
-        <ThemeProvider>
-        <ContextProvider>
-            <SuspenseLoader className="h-auto">
-                <AppBar />
-            </SuspenseLoader>
-            <section>
-                <SuspenseLoader className="h-auto">
-                    <Frame />
-                </SuspenseLoader>
-                <main className="sm:ml-72 pt-16 lg:mr-[32%] bg-white dark:bg-gray-950 min-h-screen  lg:ml-[25%]">
-                    <ErrorBoundary
-                        fallback={
-                            <SuspenseLoader>
-                                <CommonError />
-                            </SuspenseLoader>
-                        }
-                    >
-                        <SuspenseLoader>
-                            
-                    
-                                <Outlet />
-                        </SuspenseLoader>
-                    </ErrorBoundary>
-                </main>
-                <SuspenseLoader className="h-auto">
-                    <SideBar />
-                </SuspenseLoader>
-            </section>
-        </ContextProvider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <ContextProvider>
+        <AppBar />
+        <section>
+          <Frame />
+          <main className="sm:ml-72 pt-16 lg:mr-[32%] bg-white dark:bg-gray-950 min-h-screen  lg:ml-[25%]">
+            <Outlet />
+          </main>
+          <SideBar />
+        </section>
+      </ContextProvider>
+    </ThemeProvider>
+  );
 };
 
 export default FrameLayout;
