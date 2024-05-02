7f5555c128509ac61649df412433991788a8375a
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-05-02 20:24:07 +0300

    save

--- src/layouts/AppBar.tsx
+++ src/layouts/AppBar.tsx
@@ -22,7 +22,7 @@ const AppBar = () => {
   const { title, back } = useContext(TopBarContext);
 
   return (
-    <header className="fixed top-0  w-full">
+    <header className="fixed top-0 bg-gray-100 z-40 dark:bg-gray-900 py-2 px-4 w-full">
       <nav className="px-4 py-2  w-full">
         <div className="flex justify-between items-center">
           {title ? (
--- src/layouts/FrameLayout.tsx
+++ src/layouts/FrameLayout.tsx
@@ -6,6 +6,7 @@ import HomeSidebar from "../components/Home/HomeSidebar";
 import { ErrorBoundary } from "react-error-boundary";
 import CommonError from "../components/common/CommonError";
 import MainSuspense from "../components/common/MainSuspense";
+import AppBar from "./AppBar";
 
 
 
@@ -17,6 +18,9 @@ const FrameLayout = () => {
           <MainSuspense>
           <Frame />
           <div className="w-full mx-auto md:mr-[30%] md:w-[50%] sm:ml-[30%]  sm:w-[70%] xl:mr-[35%] xl:w-[45%]  md:ml-[20%]">
+            <div className="mb-16 block sm:hidden">
+              <AppBar/>
+            </div>
             <Outlet />
           </div>
           <div className="fixed hidden md:block xl:w-[35%] top-0  dark:border-gray-900 right-0 md:w-[30%]    h-screen ">
