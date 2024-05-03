c1f4f9f3201ef115bc2f9afcf487df41df226e6d
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-05-03 11:57:48 +0300

    Update PostFooter.tsx

--- src/components/Post/PostFooter.tsx
+++ src/components/Post/PostFooter.tsx
@@ -8,6 +8,7 @@ import { FaRegKissWinkHeart } from "react-icons/fa";
 import { PostType } from "../../hooks/types";
 import { MdAutoGraph } from "react-icons/md";
 import utils from "../../hooks/utils";
+import { Link } from "react-router-dom";
 
 interface Props {
   post: PostType;
@@ -61,16 +62,16 @@ const PostFooter = ({ post }: Props) => {
           <span className="normal_footer_text">{views}</span>
         </div>
       </div>
+      <div className="flex pt-3 items-center justify-between gap-x-4">
       {total_likes > 0 && (
         <>
           <div className="pt-3 flex items-center gap-x-1">
-            <a href="">liked by</a>
             {total_likes === 1 ? (
               <>
                 <div className="flex -space-x-4 rtl:space-x-reverse">
                   <a href="">
                     <img
-                      className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
+                      className="w-10 h-10  rounded-full"
                       src={post.likes[0].image||DefaultAvater}
                       alt={post.likes[0].name}
                     />
@@ -81,12 +82,12 @@ const PostFooter = ({ post }: Props) => {
               <>
                 <div className="flex -space-x-4 rtl:space-x-reverse">
                   <img
-                    className="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800"
+                    className="w-10 h-10  rounded-full"
                     src={post.likes[1].image||DefaultAvater}
                     alt={post.likes[0].name}
                   />
                   <a
-                    className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800"
+                    className="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700  rounded-full hover:bg-gray-600"
                     href="#">
                     +{total_likes-1}
                   </a>
@@ -96,6 +97,8 @@ const PostFooter = ({ post }: Props) => {
           </div>
         </>
       )}
+      <Link to={"/"} className="py-2 flex items-center justify-center border border-gray-100 dark:border-gray-800 rounded-2xl px-5">View Post</Link>
+      </div>
     </footer>
   );
 };
