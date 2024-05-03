08ddf2b3adea7586e3ab57c1ad93e3fbc41df477
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-05-03 12:08:34 +0300

    save

--- src/components/Account/VerifiedMsg.tsx
+++ src/components/Account/VerifiedMsg.tsx
@@ -1,10 +1,19 @@
+import VerifiedSvg from '../Partials/Account/VerifiedSvg';
 
 const VerifiedMsg = () => {
   return (
-    <div className="p-4">
-      <h2>Get Verified on Waanverse</h2>
+    <section className='p-6'>
+    <div className="p-6 bg-primary-100/70  border-primary-400 dark:bg-primary-900/30 border dark:border-primary-700 rounded-md flex items-start">
+      <div className='mr-2'>
+      <VerifiedSvg/>
+      </div>
+      <div>
+        <h2 className="text-xl font-semibold">Get Verified on Waanverse</h2>
+        <p className="text-lg text-gray-600">Having a verified account adds credibility to your profile.</p>
+      </div>
     </div>
-  )
+    </section>
+  );
 }
 
-export default VerifiedMsg
+export default VerifiedMsg;
