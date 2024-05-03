f1baaab79d05b6f992826de44a512236fc6fbcc9
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-05-04 01:10:17 +0300

    Update PostBody.tsx

--- src/components/Post/PostBody.tsx
+++ src/components/Post/PostBody.tsx
@@ -1,4 +1,5 @@
 import { PostType } from "../../hooks/types";
+import { Link } from "react-router-dom";
 
 interface Props{
     post: PostType;
@@ -8,6 +9,22 @@ const PostBody = ({post}: Props) => {
   return (
     <main className="pl-12">
         <p className="text-gray-900 dark:text-gray-100 whitespace-pre-wrap">{post.content}</p>
+        {post.taged_accounts && post.taged_accounts.length !== 0 && (
+          <div className="flex items-center">
+            <div className="flex items-center gap-2">
+              {post.taged_accounts.map((account) => (
+                <div key={account.id} onClick={(e) => e.stopPropagation()}>
+                  <Link
+                    to={`/${account.username}`}
+                    className="flex text-sm text-primary-600 items-center"
+                  >
+                    <p>@{account.username}</p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
     </main>
   );
 };
