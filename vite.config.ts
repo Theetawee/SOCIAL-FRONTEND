99bf8e99905dd08882750dde174278dee5aa810b
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-05-03 09:24:01 +0300

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 
 export default defineConfig({
   publicDir: "public",
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ["**/*"],
-        clientsClaim: true,
-        skipWaiting: true,
-        cleanupOutdatedCaches: true,
-      },
-      devOptions: { enabled: true },
-      injectRegister: "script-defer",
-      manifest:false,
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.js",
-      includeAssets: ["**/*"],
-    }),
   ],
   base: "./",
 });
